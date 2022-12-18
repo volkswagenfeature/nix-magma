970f408929a9cdf5266d4b71655ef51492b469f7
tristan BetaBlue-NixOS-2022 <tristan@BetaBlue-NixOS-2022.com>
2022-12-17 21:56:39 -0500

    Added pnglatex import file

---
+++ pnglatex.nix
@@ -0,0 +1,22 @@
+{
+  lib
+, buildPythonPackage
+, fetchPypi 
+}:
+
+buildPythonPackage rec {
+  pname = "pnglatex";
+  version = "1.1";
+
+  src = fetchPypi {
+    inherit pname, version;
+    hash = "sha256-CZUGDUkmttO0BzFYbGFSNMPkWzFC/BW4NmAeOwz4Y9M=";
+  };
+
+  doCheck = false;
+
+  meta = {
+    homepage = "https://github.com/MaT1g3R/pnglatex";
+    description = "a small program that converts LaTeX snippets to png";
+  }
+}
